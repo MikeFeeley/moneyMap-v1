f23d493d02bc01fa39948ff28e6846c82ea77d28
Mike Feeley <feeley@cs.ubc.ca>
2017-08-26 13:30:27 -0700

    Change future perspective from 20 years to 30.

--- code/browser/js/BudgetModel.js
+++ code/browser/js/BudgetModel.js
@@ -51,7 +51,7 @@ class BudgetModel extends Observable {
       else
         return String (Number (n[0]) + y)
     }
-    return [1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20] .map (y => {
+    return [1,2,3,4,5,6,7,8,9,10,11,12,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30] .map (y => {
       return {
         name:  addYearToName           (y),
         start: Types .dateDMY .addYear (this._budget .start, y),
