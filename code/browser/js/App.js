767186f345bef47e9ee11ec8c2763c3491e627eb
Mike Feeley <feeley@cs.ubc.ca>
2018-05-01 11:54:39 -0700

    Resolve issue with search results popup vertical position not accounting for body scroll.

--- code/browser/js/App.js
+++ code/browser/js/App.js
@@ -197,7 +197,7 @@ class App {
         this._searchInput = this._searchPopup .find ('input');
         this._searchInput [0] .addEventListener ('keydown', e => {
           if (e .keyCode == 13) {
-            TransactionHUD .search (this._searchInput .val(), this._accModel, this._varModel, $('body'), {top: 100, left: 30});
+            TransactionHUD .search (this._searchInput .val(), this._accModel, this._varModel, $('body'), {top: $('body') .scrollTop() + 100, left: 30});
             close();
           }
           if (e .keyCode == 90 && e .metaKey) {
