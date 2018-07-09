dec74c18968dcae1f6ade8b0ba1e1d90213e0f40
Mike Feeley <feeley@cs.ubc.ca>
2018-07-08 18:03:52 -0700

    Tweak.

--- code/browser/js/Init.js
+++ code/browser/js/Init.js
@@ -1,4 +1,4 @@
-const APP_VERSION_HASH   = '9c6a4e0';
+const APP_VERSION_HASH   = '464ef24';
 const APP_VERSION_STRING = '1.0.0-beta (' + APP_VERSION_HASH + ')';
 
 const INIT_browserIsCompatible = navigator .appCodeName .includes ('Mozilla') && Number (navigator .appVersion .split (' ') [0]) >= 5.0;
