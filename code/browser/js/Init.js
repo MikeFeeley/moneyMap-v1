8f48340b3c1d23c030a1639ceb25ceeb63f84358
Mike Feeley <feeley@cs.ubc.ca>
2018-07-08 19:03:54 -0700

    Undid change to field click; broke selecting text within a field and not needed since visibility did the trick.

--- code/browser/js/Init.js
+++ code/browser/js/Init.js
@@ -1,4 +1,4 @@
-const APP_VERSION_HASH   = '4fd4793';
+const APP_VERSION_HASH   = '7ee80a0';
 const APP_VERSION_STRING = '1.0.0-beta (' + APP_VERSION_HASH + ')';
 
 const INIT_browserIsCompatible = navigator .appCodeName .includes ('Mozilla') && Number (navigator .appVersion .split (' ') [0]) >= 5.0;
