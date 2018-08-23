82dfc67593b4cf0ed2042b668d728730863acac4
Mike Feeley <feeley@cs.ubc.ca>
2018-08-23 10:49:27 -0700

    Style tweak.

--- code/browser/js/Init.js
+++ code/browser/js/Init.js
@@ -1,5 +1,5 @@
 const APP_CACHE_RESOURCES = false;
-const APP_VERSION_HASH    = '39a2f87';
+const APP_VERSION_HASH    = 'c91e6c9';
 const APP_VERSION_STRING  = '1.0.0-beta (' + APP_VERSION_HASH + ')';
 
 const INIT_browserIsCompatible = navigator .appCodeName .includes ('Mozilla') && Number (navigator .appVersion .split (' ') [0]) >= 5.0;
