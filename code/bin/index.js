cca1051d16fa29c0254b15619ba96f2753ff8f54
Mike Feeley <feeley@cs.ubc.ca>
2018-09-13 16:40:58 -0700

    Update meta tags for search engine discovery.

--- code/bin/index.js
+++ code/bin/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get ('/', function(req, res, next) {
-  const head = '<title>moneyMap</title><script src="js/Init.js"></script>';
+  const head = '<title>moneyMap</title><script src="js/Init.js"></script>' +
+    '<meta name="description" content="Simple, powerful, secure and free personal financial tracking, budgeting, and planning">' +
+    '<meta author="Mike Feeley">' +
+    '<meta charset="UTF-8">';
   const html = '<!DOCTYPE html><html><head>' + head + '</head></html>';
   res .format ({
     'text/html': () => res .send (html)
