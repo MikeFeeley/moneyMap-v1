aa66f88a5ffa296ba73fc268ed4cc2a2485fc0c3
Mike Feeley <feeley@cs.ubc.ca>
2018-09-19 08:50:01 -0700

    About panel tweak.

--- code/browser/js/Preferences.js
+++ code/browser/js/Preferences.js
@@ -82,11 +82,11 @@ class Preferences extends Observable {
       .append ($('<div>', {html: '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; linearicons.com', css: {color: '#ccc'}}))
       .append ($('<div>', {html: '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; https://stuk.github.io/ (JSZip)', css: {color: '#ccc'}}))
       .append ($('<div>', {html: '&nbsp;'}))
-      .append ($('<div>', {text: 'github.com/MikeFeeley/moneyMap.git', css: {color: '#aaa', 'padding-bottom': '6px'}}))
+      .append ($('<a>', {href: 'https://github.com/MikeFeeley/moneyMap', target: '_blank', text: 'github.com/MikeFeeley/moneyMap', css: {color: '#aaa', 'padding-bottom': '6px'}}))
       .append ($('<div>', {text: '(c) 2018 Mike Feeley (feeley@cs.ubc.ca)'}))
     about.css ({top: 100, left: 50});
     ui .ModalStack .add (
-      () => {return true},
+      e => e && !$.contains (about .get (0), e .target) && about .get (0) != e .target,
       () => {about .remove()},
       true
     )
