9ece58d2df750738f4e01858aa11dda24565791f
Mike Feeley <feeley@cs.ubc.ca>
2018-10-25 15:24:45 -0700

    Attempt to resolve lingering TAB in category race condition by removing call to onSelect in hide (its now redundant, I think) ... this probably doesn't solve the problem though.

--- code/browser/js/CategoryPicker.js
+++ code/browser/js/CategoryPicker.js
@@ -118,10 +118,6 @@ class CategoryPicker {
   hide() {
     const curId = this._id || null;
     this._id = undefined;
-    var selected = this._cols
-      .map    (c => {return c.find (r => {return r.selected})})
-      .filter (c => {return c});
-    this._onSelect ((selected.length && selected.slice (- 1) [0].id) || '');
     this._view .hide();
     this._budgetProgressHUD .hide();
     if (this._filterTimeout) {
